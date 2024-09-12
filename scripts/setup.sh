253bafb816f5dec03b3b5168325f8a9d016d7265
William Guimont-Martin <wil9876543210@hotmail.com>
2024-09-12 16:17:07 -0400

    wip

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,13 +4,13 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-cp ~/glo4001/scripts/jupyter.service /etc/systemd/system/jupyter.service
-cp ~/glo4001/scripts/ros_rover.service /etc/systemd/system/ros_rover.service
+sudo cp ~/glo4001/scripts/jupyter.service /etc/systemd/system/jupyter.service
+sudo cp ~/glo4001/scripts/ros_rover.service /etc/systemd/system/ros_rover.service
 
-sudo systemctl daemon-reload
+sudo systemclt daemon-reload
 
 sudo systemclt enable jupyter
 sudo systemclt enable ros_rover
 
-sudo systemctl start jupyter
-sudo systemctl start ros_rover
+sudo systemclt start jupyter
+sudo systemclt start ros_rover

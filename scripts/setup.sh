cd591222871f3a78d13b006a2812f89e05de8c4a
William Guimont-Martin <wil9876543210@hotmail.com>
2024-09-12 16:17:46 -0400

    wip

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,10 +7,10 @@ pip install -r requirements.txt
 sudo cp ~/glo4001/scripts/jupyter.service /etc/systemd/system/jupyter.service
 sudo cp ~/glo4001/scripts/ros_rover.service /etc/systemd/system/ros_rover.service
 
-sudo systemclt daemon-reload
+sudo systemctl daemon-reload
 
-sudo systemclt enable jupyter
-sudo systemclt enable ros_rover
+sudo systemctl enable jupyter
+sudo systemctl enable ros_rover
 
-sudo systemclt start jupyter
-sudo systemclt start ros_rover
+sudo systemctl start jupyter
+sudo systemctl start ros_rover

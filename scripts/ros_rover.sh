e789d5b2ae84bf85344a565dfddf3f098bfaea33
William Guimont-Martin <wil9876543210@hotmail.com>
2024-09-12 16:47:21 -0400

    wip

--- scripts/ros_rover.sh
+++ scripts/ros_rover.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export HOME=/home/norlab
 source /opt/ros/foxy/setup.bash
 source /home/norlab/ros2_ws/install/setup.bash
 cd /home/norlab/ros2_ws/ && ros2 launch rover_launchers rover_base_launch.xml

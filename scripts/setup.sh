84c2425b040b41a54ec5d435b6267d33b20694e3
William Guimont-Martin <wil9876543210@hotmail.com>
2024-09-13 08:10:51 -0400

    removed jupyter service

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,17 +5,17 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-sudo cp ~/glo4001/scripts/jupyter.service /etc/systemd/system/jupyter.service
+#sudo cp ~/glo4001/scripts/jupyter.service /etc/systemd/system/jupyter.service
 #sudo cp ~/glo4001/scripts/ros_rover.service /etc/systemd/system/ros_rover.service
 
 sudo chmod 777 -R /tmp
 
-sudo systemctl daemon-reload
+#sudo systemctl daemon-reload
 
-sudo systemctl enable jupyter
+#sudo systemctl enable jupyter
 #sudo systemctl enable ros_rover
 
-sudo systemctl start jupyter
+#sudo systemctl start jupyter
 #sudo systemctl start ros_rover
 
 cd ~/ros2_ws

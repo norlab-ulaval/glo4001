e20817c091e9c94b71865f1afadf1fab4e114a51
William Guimont-Martin <wil9876543210@hotmail.com>
2024-09-18 19:01:25 -0400

    wip

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 sudo apt install -y python3.8-venv screen udev
 cd ~/glo4001
+git reset --hard
+git pull
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
@@ -23,4 +25,6 @@ sudo udevadm control --reload-rules && sudo udevadm trigger
 #sudo systemctl start ros_rover
 
 cd ~/ros2_ws
+git reset --hard
+git pull
 ./symlink_build.sh

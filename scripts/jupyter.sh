674359e4ce63405977bc8c1f9e3e950ec4c6ebdd
William Guimont-Martin <wil9876543210@hotmail.com>
2024-11-15 10:51:02 -0500

    shutdown jupyter on start

--- scripts/jupyter.sh
+++ scripts/jupyter.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+cd /home/norlab/glo4001/scripts || exit
+./shutdown_jupyter.sh
 cd /home/norlab/glo4001 || exit
 source venv/bin/activate
 jupyter notebook --allow-root
---
+++ scripts/shutdown_jupyter.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ps aux | grep jupyter | awk '{print $2}' | xargs kill
